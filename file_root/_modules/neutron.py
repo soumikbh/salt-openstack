c555f67dc242327ce057070d01dc07ea140e0da9
akilesh1597 <akilesh1597@gmail.com>
2014-11-25 17:51:15 +0530

    neutron status fix

--- file_root/_modules/neutron.py
+++ file_root/_modules/neutron.py
@@ -94,10 +94,7 @@ def _autheticate(func_name):
             endpoint_type='publicURL')
         neutron_interface = client.Client(
             endpoint_url=endpoint, token=token)
-        LOG.error('calling with args ' + str(args))
-        LOG.error('calling with kwargs ' + str(nkwargs))
         return_data = func_name(neutron_interface, *args, **nkwargs)
-        LOG.error('got return data ' + str(return_data))
         if isinstance(return_data, list):
             # format list as a dict for rendering
             return {data.get('name', None) or data['id']: data
